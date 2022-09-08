9dbe55ead7ed45b89cb037c4e2177b9fabd583d1
6ladeus6 <89492611+6ladeus6@users.noreply.github.com>
2022-09-07 20:18:24 -0500

    Final Cine V1

--- Cine/src/controller/controladorIndex.js
+++ Cine/src/controller/controladorIndex.js
@@ -1,23 +1,114 @@
 let peliculas=[
     {
-        nombre:"Avatar",
+        nombre:"avatar",
         genero:"Ciencia ficción",
         duracion:220,
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemiercolesjjg.appspot.com/o/avatar.png?alt=media&token=4775b8fa-5e9c-42bb-9ae3-f0f0bf010f0d",
-        sinopsis:"loremjasjsadjsajsdajsajsdajsda",
+        sinopsis:"Entramos en el mundo Avatar de la mano de Jake Sully, un ex-Marine en silla de ruedas, que ha sido reclutado para viajar a Pandora, donde existe un mineral raro y muy preciado que puede solucionar la crisis energética existente en la Tierra",
         clasificacion:"+18",
-        idioma:"En subtitulado"
+        idioma:"En subtitulado",
+        director:"James Cameron",
+        reparto:"Sam Worthington,Zoe Saldaña,Michelle Rodriguez"
     },
     {
         nombre:"La Bestia",
-        genero:"Suspenso",
+        genero:"suspenso",
         duracion:120,
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemiercolesjjg.appspot.com/o/la%20bestia.jpg?alt=media&token=b46a53b9-3af7-4fc2-9c7e-966ec628f719",
-        sinopsis:"loremjasjsadjsajsdajsajsdajsda",
+        sinopsis:"Un hombre que acaba de enviudar y sus dos hijas adolescentes viajan a una reserva de caza en Sudáfrica. Sin embargo, su viaje de curación pronto se convierte en una lucha por la supervivencia cuando un león sediento de sangre comienza a acecharlos.",
         clasificacion:"+18",
-        idioma:"En subtitulado"
+        idioma:"En subtitulado",
+        director:"Baltasar Kormákur",
+        reparto:"Idris Elba,Sharlto Copley,Iyana Halley"
+    },
+    {
+        nombre:"Un parcero en NY",
+        genero:"Comedia",
+        duracion:90,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/ny.png?alt=media&token=488c885a-08ed-4ac8-a216-3522075f0dce",
+        sinopsis:"Entramos en el mundo Avatar de la mano de Jake Sully, un ex-Marine en silla de ruedas, que ha sido reclutado para viajar a Pandora, donde existe un mineral raro y muy preciado que puede solucionar la crisis energética existente en la Tierra",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"Harold Trompetero",
+        reparto:"Carlos Hurtado Beltrán,Judith Segura,Walter Luengas"
+    },
+    {
+        nombre:"Telefono Negro",
+        genero:"Terror",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/telefono.png?alt=media&token=17002d7e-26cd-4721-9ee7-afa71ab436b1",
+        sinopsis:"Un hombre que acaba de enviudar y sus dos hijas adolescentes viajan a una reserva de caza en Sudáfrica. Sin embargo, su viaje de curación pronto se convierte en una lucha por la supervivencia cuando un león sediento de sangre comienza a acecharlos.",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"Pablo Matin",
+        reparto:"Seaw Degan, Jhon Doe, Mario Cimarro"
+    },
+    {
+        nombre:"Thor",
+        genero:"Ciencia ficción",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/thor.png?alt=media&token=5ede5d53-945f-4dd6-9f46-fa62cd257a01",
+        sinopsis:"Entramos en el mundo Avatar de la mano de Jake Sully, un ex-Marine en silla de ruedas, que ha sido reclutado para viajar a Pandora, donde existe un mineral raro y muy preciado que puede solucionar la crisis energética existente en la Tierra",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Steven Spilberg",
+        reparto:"Crish Evans, Thor, Tony Stark"
+    },
+    {
+        nombre:"Jack",
+        genero:"Terror",
+        duracion:160,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/jack.png?alt=media&token=f94c0f31-8f05-42da-bc1a-fb806c8083c1",
+        sinopsis:"Un hombre que acaba de enviudar y sus dos hijas adolescentes viajan a una reserva de caza en Sudáfrica. Sin embargo, su viaje de curación pronto se convierte en una lucha por la supervivencia cuando un león sediento de sangre comienza a acecharlos.",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Jack Daniels",
+        reparto:"Pablo Nerudo, Mario Atencia, Andres Gomez"
+    },
+    {
+        nombre:"Sana que Sana",
+        genero:"Comedia",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/sana.png?alt=media&token=65e38775-5f17-4b2c-ad61-030f46a7b2dc",
+        sinopsis:"Entramos en el mundo Avatar de la mano de Jake Sully, un ex-Marine en silla de ruedas, que ha sido reclutado para viajar a Pandora, donde existe un mineral raro y muy preciado que puede solucionar la crisis energética existente en la Tierra",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"Cristian Thonsoop",
+        reparto:"Suso el Paspi, Martin Robledo, Laura Acuña"
+    },
+    {
+        nombre:"Dragon Ball Z",
+        genero:"Suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/dbz.jpg?alt=media&token=efadf672-7fb2-4e75-851b-9daa6b86c493",
+        sinopsis:"Un hombre que acaba de enviudar y sus dos hijas adolescentes viajan a una reserva de caza en Sudáfrica. Sin embargo, su viaje de curación pronto se convierte en una lucha por la supervivencia cuando un león sediento de sangre comienza a acecharlos.",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Akira Torillama",
+        reparto:"Goku, Picoro, Gohan"
+    },
+    {
+        nombre:"Lopeardo de las nieves",
+        genero:"Suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/leo.png?alt=media&token=0dc649aa-efc4-4c5f-b12f-e32c677e0cfd",
+        sinopsis:"Un hombre que acaba de enviudar y sus dos hijas adolescentes viajan a una reserva de caza en Sudáfrica. Sin embargo, su viaje de curación pronto se convierte en una lucha por la supervivencia cuando un león sediento de sangre comienza a acecharlos.",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Jhon Doe",
+        reparto:"Pedro Pablo, Andres Garcia, Alejandro Ladeus"
+    },
+    {
+    nombre:"Minions",
+    genero:"Animada",
+    duracion:120,
+    poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/minio.jpg?alt=media&token=bd0a09ff-006c-4605-b206-ebc9fbe44587",
+    sinopsis:"Un hombre que acaba de enviudar y sus dos hijas adolescentes viajan a una reserva de caza en Sudáfrica. Sin embargo, su viaje de curación pronto se convierte en una lucha por la supervivencia cuando un león sediento de sangre comienza a acecharlos.",
+    clasificacion:"+18",
+    idioma:"En subtitulado",
+    director:"Disney",
+    reparto:"Minion 1, Minion 2, Mion 3"
     }
-   
 ];
 let fila = document.getElementById("fila");
 
@@ -50,15 +141,37 @@ peliculas.forEach(function(pelicula){
   let generoPelicula = document.createElement("h4");
   generoPelicula.classList.add("card-text");
   generoPelicula.textContent = "Genero: " + pelicula.genero;
-  //6. AGREGAMOS EL IDIOMA
-  let idiomaPelicula = document.createElement("h5");
-  idiomaPelicula.classList.add("fw-bold");
-  idiomaPelicula.textContent = ("card-text");
+//6. AGREGAMOS EL IDIOMA
+    let idioma=document.createElement("h6");
+    idioma.classList.add("fw-bold");
+    idioma.textContent= "Idioma: " + pelicula.idioma;
+//7. AGREGAMOS LA SINOPSIS
+    let sinopsis=document.createElement("p");
+    sinopsis.classList.add("card-text");
+    sinopsis.textContent=pelicula.sinopsis;
+ //8. AGREGAMOS LA CLASIFICACIÓIN
+    let clasificacion = document.createElement("h6");
+    clasificacion.classList.add("fw-bold");
+     clasificacion.textContent = "Clasificación: " + pelicula.clasificacion;
+//9. DIRECTOR
+let director = document.createElement("h6");
+    director.classList.add("fw-bold");
+     director.textContent = "Director: " + pelicula.director;
+//10. REPARTO
+let reparto = document.createElement("h6");
+    reparto.classList.add("fw-bold");
+     reparto.textContent = "Reparto: " + pelicula.reparto;
 
     //PADRES E HIJOS
     card.appendChild(poster);
     card.appendChild(nombrePelicula);
     card.appendChild(generoPelicula);
+    card.appendChild(idioma);
+    card.appendChild(clasificacion);
+    card.appendChild(director);
+    card.appendChild(reparto);
+    card.appendChild(sinopsis);
+    
    columna.appendChild(card);
    fila.appendChild(columna);
 
@@ -69,4 +182,127 @@ peliculas.forEach(function(pelicula){
 //DETECTANDO SELECCIÓN DE UNA PELICULA
 fila.addEventListener('click',function(){
     alert("esta seleccionando una pelicula");
+});
+
+///---------------------------------------------------------PELICULAS PROXIMAS A ESTRENO---------------------------------------------------------------
+let proximas = [
+    {
+        nombre:"Gallo de Pelea",
+        genero:"Drama",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/1.jpg?alt=media&token=159eadcb-2557-41fb-bcb9-7ebe82eccfa9",
+        sinopsis:"A raíz del extraño asesinato de su hijo y después de afrontar muchos años en soledad desde el fallecimiento de su esposa, Gilberto, un humilde y silencioso cultivador, decide ponerle cara a su destino, inspirado por la valentía de un gallo de pelea que misteriosamente aparece en su vida.",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"Cameron Max",
+        reparto:"Sam Kingstong,Mario Saldaña,Andrea Rodriguez"
+    },
+    {
+        nombre:"Erase una vez un Genio",
+        genero:"Comedia",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/2.png?alt=media&token=18d165e2-9647-459c-a59b-043ceaa385ec",
+        sinopsis:"Una aventura fantástica donde una mujer, Alithea(Tilda Swinton) descubre un genio (Idris Elba) en una lámpara. Ella es consciente que ninguna historia sobre deseos concedidos tiene un final feliz, así se niega a pedir sus 3 deseos que liberarían al genio. Él, en un esfuerzo por convencerla, le cuenta sus aventuras a través del tiempo entre épocas de reyes y faraones y cómo el haber amado a la mujer equivocada lo obligó a convertirse en prisionero de esta botella, solo, por la eternidad. Él anhela libertad, ella anhela compañía, quizás encuentran en el otro lo que siempre anhelaron y necesitaron",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Baltasar Kormákur",
+        reparto:"Idris Marcia,Mario Copley,Diana Marcela Halley"
+    },
+    {
+        nombre:"Peleando por mi vida",
+        genero:"Acción",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/3.png?alt=media&token=c955c5dc-e69b-492c-988d-5d0447812ddd",
+        sinopsis:"Después de la Segunda Guerra Mundial, Harry Haft es un boxeador que luchó contra sus compañeros prisioneros en los campos de concentración para sobrevivir. Atormentado por los recuerdos y la culpa, intenta usar peleas de alto perfil contra leyendas del boxeo como Rocky Marciano como una forma de encontrar a su primer amor nuevamente.",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"Barry Levinson",
+        reparto:"Ben Foster, Billy Magnussen, Vicky Krieps"
+    },
+    {
+        nombre:"Psaje al paraiso",
+        genero:"Drama",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/4.png?alt=media&token=697f19b2-cbb4-4207-b64f-18ccdc06bc62",
+        sinopsis:"os ganadores del Premio de la Academia® George Clooney y Julia Roberts se reúnen en la pantalla grande como ex esposos que comparten la misión de evitar que su enamorada hija cometa el mismo error que ellos mismos cometieron una vez. De Working Title, Smokehouse Pictures y Red Om Films, Pasaje al Paraíso es una comedia romántica sobre la dulce sorpresa de las segundas oportunidades.",
+        clasificacion:"+18",
+        idioma:"Inglés",
+        director:"Ol Parker.",
+        reparto:"George Clooney, Julia Roberts."
+    },
+    {
+        nombre:"La Ciudad de las Fieras",
+        genero:"Ciencia ficción",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineal.appspot.com/o/5.png?alt=media&token=32f42fa9-78a3-4cc8-9086-67b66f113ba0",
+        sinopsis:"Tras la muerte de su madre, Tato, un joven amante del rap, se rehúsa a dejar Medellín para vivir en el campo con un abuelo que no sabía que tenía. Ante una ciudad que poco a poco revela una gran hostilidad, Tato ha encontrado en la música y en su pequeño grupo de amigos un refugio. Sin embargo, esto no es suficiente y tendrá que vivir bajo el techo y las reglas de don Octavio, un viejo campesino floricultor de fuertes tradiciones. Nieto y abuelo descubren así una relación fundada en los lazos familiares, pero también en una pérdida común y el consuelo que los dos encuentran en la música.",
+        clasificacion:"+18",
+        idioma:"Español",
+        director:"Henry E. Rincón.",
+        reparto:"Bryan Córdoba,Oscar Atehortúa,Valeria Pérez"
+    }
+
+];
+
+
+let fila2 = document.getElementById("fila2");
+proximas.forEach(function(proximamente){
+
+    
+    //TRAVERSING (CREAR ETIQUETAS DE HTML DESDE JS)
+   //1. CREAMOS UNA COLUMNA PARA CADA PELICULA
+   let columna2 = document.createElement("div");
+   columna2.classList.add("col");
+   //2.CREAMOS UNA TARJETA PARA CADA PELICULA
+   let card2 = document.createElement("div");
+   card2.classList.add("card", "h-100");
+  //3. CREAMOS UNA FOTO PARA CADA PELICULA
+  let poster = document.createElement("img");
+  poster.classList.add("card-img-top");
+  poster.src = proximamente.poster;
+  //4. AGREGAMOS LOS NOMBRES A LAS PELICULAS
+  let nombrePelicula = document.createElement("h3");
+  nombrePelicula.classList.add("card-title","text-center");
+  nombrePelicula.textContent = proximamente.nombre;
+  //5. AGREGAMOS EL GENERO DE LAS PELICULAS
+  let generoPelicula = document.createElement("h4");
+  generoPelicula.classList.add("card-text");
+  generoPelicula.textContent = "Genero: " + proximamente.genero;
+//6. AGREGAMOS EL IDIOMA
+    let idioma=document.createElement("h6");
+    idioma.classList.add("fw-bold");
+    idioma.textContent= "Idioma: " + proximamente.idioma;
+//7. AGREGAMOS LA SINOPSIS
+    let sinopsis=document.createElement("p");
+    sinopsis.classList.add("card-text");
+    sinopsis.textContent=proximamente.sinopsis;
+ //8. AGREGAMOS LA CLASIFICACIÓIN
+    let clasificacion = document.createElement("h6");
+    clasificacion.classList.add("fw-bold");
+     clasificacion.textContent = "Clasificación: " + proximamente.clasificacion;
+//9. DIRECTOR
+let director = document.createElement("h6");
+    director.classList.add("fw-bold");
+     director.textContent = "Director: " + proximamente.director;
+//10. REPARTO
+let reparto = document.createElement("h6");
+    reparto.classList.add("fw-bold");
+     reparto.textContent = "Reparto: " + proximamente.reparto;
+
+    //PADRES E HIJOS
+    card2.appendChild(poster);
+    card2.appendChild(nombrePelicula);
+    card2.appendChild(generoPelicula);
+    card2.appendChild(idioma);
+    card2.appendChild(clasificacion);
+    card2.appendChild(director);
+    card2.appendChild(reparto);
+    card2.appendChild(sinopsis);
+    
+   columna2.appendChild(card2);
+   fila2.appendChild(columna2);
+
+
+
+
 });
